194d51df9e2f7d2dcc54aaad6429a84bbb2ae449
ariaandika <andikaakun17@gmail.com>
2024-04-02 18:09:26 +0700

    add users create read

--- controllers/users.js
+++ controllers/users.js
@@ -2,5 +2,13 @@ import express from "express"
 
 export const users = express.Router()
 
-users.get("/", (req,res) => res.send("Express Dev!"))
+users.get("/", (req,res) => {
+    res.json({ users: ["John","Doe"] })
+})
+
+users.post("/", (req,res) => {
+    console.log("user post: ", req.body)
+    res.sendStatus(201)
+})
+
 
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import { users } from "./controllers/users.js"
+import parser from "body-parser"
 
 const app = express()
 
+app.use(parser.json())
 app.use("/users",users)
 
 app.listen(3000)
