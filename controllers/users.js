6144b9e9bfea41140f9e396774e585011a2206b2
ariaandika <andikaakun17@gmail.com>
2024-04-02 21:54:47 +0700

    setup prisma

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,19 @@
 import express from "express"
+import { prisma } from "../lib/prisma.js"
 
 export const users = express.Router()
 
-users.get("/", (req,res) => {
-    res.json({ users: ["John","Doe"] })
+users.get("/", async (req,res) => {
+    const users = await prisma.users.findMany()
+    res.json(users)
 })
 
-users.post("/", (req,res) => {
-    console.log("user post: ", req.body)
+users.post('/', async (req,res) => {
+    await prisma.users.create({
+        data: req.body
+    })
     res.sendStatus(201)
 })
+
 
 
